21f8223fd8974d9a54f5b20c6f2274902f1d8813
RJones2019 <rakeem@usca.edu>
2021-11-03 13:02:11 -0400

    1 file modified

--- js/add-element.js
+++ js/add-element.js
@@ -3,21 +3,21 @@
  */
 
 // get all the hot class elements- change them to cool class
-$(".hot").each(function(){
-  $(this).removeClass("hot");
-  $(this).addClass("cool");
+$('.hot').each(function(){
+  $(this).removeClass('hot');
+  $(this).addClass('cool');
 });
 // traverse the elements
-
+$('#one').next().next().text('milk');
 // add a new element by clicking the plus sign
-
+$('#todo').append("<li>candy</li>");
 // before and after are for siblings
 // append and prepend are for parent
-
+$('#add').click(addElement);
 function addElement() {
   // add a new element
   // add a input text box
-  
+  $('#todo').append("<li>user</li>");
   // whenever the user are done add the element
   
 }
